2f7bb482ce392f179c1e111a4b3924f3ce28c70f
Sagar Singh <sssssagarsingh24@gmail.com>
2024-12-25 10:34:26 +0000

    datatypes

---
+++ Basics_js/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict"; // trat all js code as new version
+
+// alert (3+3) //we are using node.js, not browser
+
+console.log(3
+    +
+    3)//code readeability  should be high
+
+    console.log("Sagar")
+
+    let name = "Sagar"
+    let age = 23
+    let isLoginEnd = true
+    let state;
+
+    //number => 2 to the power 53
+    //bigInt
+    //string => ""
+    //boolean => true/false
+    //null => standalone value
+    //undefined
+    //symbol => unique
+
+
+    //object
+
+    console.log(typeof "Sagar");
+    console.log(typeof 25);
+    console.log(typeof true);
+    console.log(typeof null);//object
+    console.log(typeof undefined);//Undefined
+    
