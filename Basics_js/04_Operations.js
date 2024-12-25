553c391f785551dff50c7415fed58a862e0a79c0
Sagar Singh <sssssagarsingh24@gmail.com>
2024-12-25 17:54:40 +0530

    Add files via upload

---
+++ Basics_js/04_Operations.js
@@ -0,0 +1,35 @@
+let value = 3
+let negValue = -value
+console.log((negValue));
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+console.log(2^2);
+
+let str1 = "hello"
+let str2 = " Sagar"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
