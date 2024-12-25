272117003bbd8f98c545a2752c7984377dc1f72f
Sagar Singh <sssssagarsingh24@gmail.com>
2024-12-25 16:44:59 +0530

    Add files via upload

---
+++ Basics_js/ConversionDatatypes.js
@@ -0,0 +1,45 @@
+let score = "23abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//****************************************************************************
+let flag = null
+
+console.log(typeof flag);
+console.log(typeof (flag));
+
+let flag0 = Number(flag)
+console.log(typeof flag0);
+console.log(flag0);
+
+//*****************************************************************************
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1; false => 0;
+
+
+let isLoggedIn = 1
+//let isLoggedIn = ""
+//let isLoggedIn = "Sagar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false;
+// "Sagar" => true;
+
+//****************************************************************** */
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
