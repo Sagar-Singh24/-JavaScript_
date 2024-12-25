34ae97f4499e1b2067a0bda6cf4fde54ffb7518a
Sagar Singh <sssssagarsingh24@gmail.com>
2024-12-25 10:03:59 +0000

    New Contribute done

---
+++ Basics_js/01_Variables.js
@@ -0,0 +1,21 @@
+const accountId = 12345
+let accountEmail = "sagarsingh@gmail.com"
+var accountPassword = "01234"
+accountCity = "NewYork"
+
+
+//accountId = 2 //Not Allowed
+
+accountEmail =  "Sa@Sa.com"
+accountPassword = "12121212"
+accountCity = "Bengaluru"
+let accountState;
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) //In table form
+
+/*
+Prefer not to use var
+beacuse of issue in block scope and functional scope
+*/
