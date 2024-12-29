4b5bb4b3f1a212558f0d12e369bed127bb84d66e
Sagar Singh <sssssagarsingh24@gmail.com>
2024-12-29 19:26:33 +0530

    Update 04_Operations.js

--- Basics_js/04_Operations.js
+++ Basics_js/04_Operations.js
@@ -16,7 +16,7 @@ let str2 = " Sagar"
 let str3 = str1 + str2
 console.log(str3);
 
-console.log("1" + 2);
+console.log("1" + 2)
 console.log(1 + "2")
 console.log("1" + 2 + 2)
 console.log(1 + 2 + "2")
