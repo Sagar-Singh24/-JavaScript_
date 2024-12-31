e56e38f09168dc7ba7ea95b66572f2b388e9e5a6
Sagar Singh <sssssagarsingh24@gmail.com>
2024-12-31 19:25:34 +0530

    Add files via upload

---
+++ Basics_js/07_Stack & Heap.js
@@ -0,0 +1,22 @@
+// Stack(Primitive)    Heap(Non-Primitive)
+
+let myYoutubename = "Sagar Singh"
+
+let anothername = myYoutubename
+anothername = "SinghCode"
+
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sagar@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
