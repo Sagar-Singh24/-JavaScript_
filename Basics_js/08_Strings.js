12768061f0e69f61c8b860b42e1e54ded5f6c685
Sagar Singh <sssssagarsingh24@gmail.com>
2024-12-31 19:26:15 +0530

    Add files via upload

---
+++ Basics_js/08_Strings.js
@@ -0,0 +1,38 @@
+const name = "Sagar"
+const repoCount = 20
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is 
+    ${repoCount}`);
+
+const gameName = new String('SagarSingh')
+
+    // console.log(gameName[0]);
+    // console.log(gameName.__proto__);
+    // console.log(gameName.length);
+    // console.log(gameName.toUpperCase());
+    // console.log(gameName.charAt(2));
+    // console.log(gameName.indexOf('g'));
+
+    const newString = gameName.substring(0,4)
+    console.log(newString);
+
+    const anotherString = gameName.slice(-8,4)
+    console.log(anotherString);
+    
+
+const newStringOne = " Sagar "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://chatgpt.com/"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('Sundar'));
+
+console.log(gameName.split('-'));
+
+
+    
