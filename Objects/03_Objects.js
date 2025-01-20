392591dba30754c284a0a7ea0e2044c998415434
Sagar Singh <sssssagarsingh24@gmail.com>
2025-01-20 19:51:26 +0530

    Add files via upload

---
+++ Objects/03_Objects.js
@@ -0,0 +1,20 @@
+const course = {
+    courseName: "JavaScript",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+//course.courseInstructor
+
+const {courseInstructor:instructor} = course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+//APIs
+// {
+//     "name" = "Sagar",
+//     "courseName" = "Javascript",
+//     "Price" = "Free"
+// }
+
