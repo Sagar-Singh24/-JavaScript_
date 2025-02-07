c3948e6206be9e15fce3dc7f9bfcc5857ed3813e
Sagar Singh <sssssagarsingh24@gmail.com>
2025-02-07 22:29:19 +0530

    Update Maths.js

--- Maths&Num/Maths.js
+++ Maths&Num/Maths.js
@@ -1,7 +1,7 @@
 console.log(Math)
 console.log(Math.abs(-5))
 console.log(Math.round(4.3))
-console.log(Math.ceil(4.2));
+console.log(Math.ceil(4.2))
 console.log(Math.floor(4.9));
 console.log(Math.min(4,7,2,9));
 console.log(Math.max(4,7,2,9));
