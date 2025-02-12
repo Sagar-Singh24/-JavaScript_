d20d121f6b640881010c79d0fc1480800fc9593e
Sagar Singh <sssssagarsingh24@gmail.com>
2025-02-12 22:34:36 +0530

    Update 02_Object.js

--- Objects/02_Object.js
+++ Objects/02_Object.js
@@ -53,7 +53,7 @@ console.log(tinderUser);
 
 console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser))
+console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLogged'))
 
