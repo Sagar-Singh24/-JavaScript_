a69a59df620bec80350190acb839559688035fc7
Sagar Singh <sssssagarsingh24@gmail.com>
2025-02-13 22:18:32 +0530

    Update 09_Date.js

--- Basics_js/09_Date.js
+++ Basics_js/09_Date.js
@@ -28,7 +28,7 @@ console.log(Math.floor(Date.now()/1000));
 let newDate = new Date()
 console.log(newDate);
 console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+console.log(newDate.getDay())
 
 newDate.toLocaleString('default',{
     weekday:"long"
