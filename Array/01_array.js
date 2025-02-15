87d687d1e1d95809fe18c5c22df0b5646774061b
Sagar Singh <sssssagarsingh24@gmail.com>
2025-02-15 16:25:16 +0530

    Update 01_array.js

--- Array/01_array.js
+++ Array/01_array.js
@@ -35,7 +35,7 @@ console.log("A" , myArr);
 const myn1 = myArr.slice(1,3)
 
 console.log(myn1);
-console.log("B", myArr);
+console.log("B", myArr)
 
 const myn2 = myArr.splice(1,3)
 console.log("C", myArr)
