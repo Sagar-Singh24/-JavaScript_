ea79e01182d1ca681fd246c8f082d340be864a96
Sagar Singh <sssssagarsingh24@gmail.com>
2025-02-21 23:09:15 +0530

    Update Num.js

--- Maths&Num/Num.js
+++ Maths&Num/Num.js
@@ -4,7 +4,7 @@ console.log(score);
 const balance = new Number(100)
 console.log(balance);
 
-console.log(balance.toString().length);
+console.log(balance.toString().length)
 console.log(balance.toFixed(2))
 
 const otherNumber = 123.8966
